1ac5d284178f48a427b33aa9d38055ee2d8a5f1c
Stephane HERMAN <stephane.herman@live.fr>
2023-08-20 17:53:05 +0200

    ✨ Implement base DTO with validation

---
+++ src/dto.ts
@@ -0,0 +1,23 @@
+import { instanceToPlain } from 'class-transformer';
+import { validateOrReject } from 'class-validator';
+import { Plain } from './plain.type';
+
+export class Dto {
+  async validate() {
+    console.debug(`Validating ${this.constructor.name}...`);
+    try {
+      await validateOrReject(this, {
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      });
+    } catch (errors) {
+      console.debug(JSON.stringify(errors, null, 2));
+
+      throw errors;
+    }
+  }
+
+  toPlainObject(): Plain<this> {
+    return instanceToPlain(this) as Plain<this>;
+  }
+}
