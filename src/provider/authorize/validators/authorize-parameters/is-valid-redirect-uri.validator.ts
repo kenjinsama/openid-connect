908e5b9e777afa4559317feb5ecf35d88df1c0df
Stephane HERMAN <stephane.herman@live.fr>
2023-08-25 16:30:27 +0200

    🦺 Implements "redirec_uri" validator

---
+++ src/provider/authorize/validators/authorize-parameters/is-valid-redirect-uri.validator.ts
@@ -0,0 +1,48 @@
+import { ValidationOptions, registerDecorator } from 'class-validator';
+import { ValidationArgumentsGeneric } from '../../../../utils/types/validation-arguments-generic.type';
+import { Client } from '../../../config/dtos/service-provider.dto';
+import { AuthorizeParametersDto } from '../../dtos/authorize-parameters.dto';
+
+export function validate(
+  value: string,
+  { object }: ValidationArgumentsGeneric<AuthorizeParametersDto>,
+) {
+  const client: Client | undefined = Reflect.getMetadata('__client', object);
+
+  return client?.redirect_uris?.includes(value) || false;
+}
+
+export function defaultMessage(
+  args: ValidationArgumentsGeneric<AuthorizeParametersDto>,
+) {
+  const client: Client | undefined = Reflect.getMetadata(
+    '__client',
+    args.object,
+  );
+
+  return `"${
+    args.object[args.property]
+  }" is an not a valid redirect_uri for client "${
+    client?.client_id || 'invalid_client'
+  }"`;
+}
+
+// Declarative code only
+/* istanbul ignore next */
+export function IsValidRedirectUri(
+  validationOptions?: ValidationOptions,
+): PropertyDecorator {
+  return function (object: Object, propertyName: string | Symbol) {
+    registerDecorator({
+      name: 'isValidRedirectUri',
+      target: object.constructor,
+      propertyName: propertyName.toString(),
+      constraints: [],
+      options: validationOptions,
+      validator: {
+        validate,
+        defaultMessage,
+      },
+    });
+  };
+}
---
+++ test/provider/authorize/validators/authorize-parameters/is-valid-redirect-uri.validator.spec.ts
@@ -0,0 +1,94 @@
+import { AuthorizeParametersDto } from '../../../../../src/provider/authorize/dtos/authorize-parameters.dto';
+import {
+  defaultMessage,
+  validate,
+} from '../../../../../src/provider/authorize/validators/authorize-parameters/is-valid-redirect-uri.validator';
+import { ValidationArgumentsGeneric } from '../../../../../src/utils/types/validation-arguments-generic.type';
+import { configMock } from '../../../config/config.service.mock';
+
+describe('IsValidRedirectUri', () => {
+  const [clientMock] = configMock.clients;
+  const redirectUriMock = clientMock.redirect_uris[0];
+
+  beforeEach(() => {
+    jest.restoreAllMocks();
+
+    jest.spyOn(Reflect, 'getMetadata').mockReturnValue(clientMock);
+  });
+
+  describe('validate', () => {
+    const authorizeParametersMock = {
+      redirect_uri: redirectUriMock,
+    };
+
+    const validationParameters = {
+      object: authorizeParametersMock,
+    } as ValidationArgumentsGeneric<AuthorizeParametersDto>;
+
+    it('should return true if the value is a valid redirect_uri', () => {
+      // When
+      const result = validate(
+        authorizeParametersMock.redirect_uri,
+        validationParameters,
+      );
+
+      // Then
+      expect(result).toBe(true);
+    });
+
+    it('should return false if the value is not a valid redirect_uri', () => {
+      // When
+      const result = validate('https://invalid.com', validationParameters);
+
+      // Then
+      expect(result).toBe(false);
+    });
+
+    it('should return false if there is no client', () => {
+      jest.spyOn(Reflect, 'getMetadata').mockReturnValueOnce(undefined);
+
+      // When
+      const result = validate(
+        authorizeParametersMock.redirect_uri,
+        validationParameters,
+      );
+
+      // Then
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('defaultMessage', () => {
+    const authorizeParametersMock = {
+      redirect_uri: redirectUriMock,
+    };
+
+    const validationParameters = {
+      object: authorizeParametersMock,
+      property: 'redirect_uri',
+    } as ValidationArgumentsGeneric<AuthorizeParametersDto>;
+
+    it('should return the default message', () => {
+      // When
+      const result = defaultMessage(validationParameters);
+
+      // Then
+      expect(result).toBe(
+        `"${authorizeParametersMock.redirect_uri}" is an not a valid redirect_uri for client "${clientMock.client_id}"`,
+      );
+    });
+
+    it('should return the default message with "invalid_client" if there is no client', () => {
+      // Given
+      jest.spyOn(Reflect, 'getMetadata').mockReturnValueOnce(undefined);
+
+      // When
+      const result = defaultMessage(validationParameters);
+
+      // Then
+      expect(result).toBe(
+        `"${authorizeParametersMock.redirect_uri}" is an not a valid redirect_uri for client "invalid_client"`,
+      );
+    });
+  });
+});
