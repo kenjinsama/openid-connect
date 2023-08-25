eceb5ca8f6e7caba96850d8ff9a7575dd214143e
Stephane HERMAN <stephane.herman@live.fr>
2023-08-25 12:17:37 +0200

    🏷️  Greatly improves Plain type with better generic signature

--- src/utils/types/plain.type.ts
+++ src/utils/types/plain.type.ts
@@ -1,6 +1,28 @@
 /* istanbul ignore file */
 
 // Declarative file
-export type Plain<T> = {
-  [K in keyof T as T[K] extends Function ? never : K]: Plain<T[K]>;
-};
+import { Primitive } from './primitive.type';
+
+type Methods<T> = {
+  // For each K in T
+  [K in keyof T]: T[K] extends Function ? K : never; // If T[K] is a function, include it, else, exclude it
+}[keyof T]; // Get the union of all function names
+
+type NonPrimitive<T> = T extends Primitive ? never : T;
+
+/**
+ * @warning This type preserves primitives and arrays methods
+ */
+export type Plain<T> = Omit<
+  {
+    // For each K in T
+    [K in keyof T]: T[K] extends Array<infer E> // If T[K] is an array, infer E its element
+      ? E extends NonPrimitive<E> // If E is any non primitive
+        ? Array<Plain<E>> // Then we need to Plain<E> too
+        : T[K] // Else, E is a primitive, then T[K] is a primitive array and we can just use it as is
+      : T[K] extends NonPrimitive<T[K]> // Else if T[K] is any non primitive
+      ? Plain<T[K]> // Then we need to Plain<E> too
+      : T[K]; // Else, T[K] is a primitive, we can just use it as is
+  },
+  Methods<T> // Exclude all functions from the resulting type
+>;
